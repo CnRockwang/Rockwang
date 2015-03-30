f9f26c74c28572efc750b005a8889d8dd3e27f47
CnRockwang <xin265755@hotmail.com>
2015-03-30 10:36:27 +0800

    test
    test

--- Program.cs
+++ Program.cs
@@ -19,9 +19,9 @@ namespace JunglePosition
             Config = new Menu("Rockwang_Saysomthing", "Saysomething", true);
             Config.AddItem(new MenuItem("Show", "显示"));
             Config.AddToMainMenu();
-	    Game.Say("小心上路miss,小心上路miss,小心上路miss,")
-	    Game.PrintChat("Rocktest11111111111111111111111");
-	    Console.WriteLine("Rocktest22222222222222222222");
+            Game.Say("小心上路miss,小心上路miss,小心上路miss,");
+            Game.PrintChat("Rocktest11111111111111111111111");
+            Console.WriteLine("Rocktest22222222222222222222");
 
            
         }
