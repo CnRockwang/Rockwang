3a2aa10a6f9b73cc80d45b069434ce4cc31037c0
CnRockwang <xin265755@hotmail.com>
2015-03-30 11:23:08 +0800

    test
    test

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ namespace JunglePosition
 
 
 
-            if (_menu.Item("Top_miss").GetValue<bool>())
+            if (_menu.Item("clcktop").GetValue<bool>())
             {
                 Game.Say("top miss,careful~~");
 
